efeb981dddedb1754be3aef42673e75542bc84b9
claw <themandibleclaw@gmail.com>
2026-01-28 23:17:07 +0000

    implement requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(self,
+                 name: str,
+                 appetite: int,
+                 is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
